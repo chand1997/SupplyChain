77edc938ff87a599362609e2890bdbf9679d85af
chand1997 <chanduchallahalli@gmail.com>
2022-12-18 14:22:58 +0530

    created gridPane.. added email and password textfield and placeholder in gridpane.. and added that gridpane to the Pane

--- src/main/java/com/example/supplychain/HelloApplication.java
+++ src/main/java/com/example/supplychain/HelloApplication.java
@@ -3,14 +3,35 @@ package com.example.supplychain;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private GridPane loginPage(){
+        Label emailLabel=new Label("Email");
+        Label passwordLabel=new Label("Password");
+
+        TextField emailTextField=new TextField();
+        PasswordField passwordField=new PasswordField();
+
+        GridPane pane=new GridPane();
+        pane.add(emailLabel,0,0);
+        pane.add(emailTextField,1,0);
+        pane.add(passwordLabel,0,1);
+        pane.add(passwordField,1,1);
+
+        return pane;
+
+    }
     private Pane createContent(){
         Pane root=new Pane();
+        root.getChildren().addAll(loginPage());
         return root;
     }
     @Override
